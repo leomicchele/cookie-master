9fb994adbac6a95fdceac9e2af2105f5f3365876
Lean Note Teco <LGMicchele@teco.com.ar>
2023-01-16 16:46:02 -0300

    Themes installed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { darkTheme } from '@/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  
+  )
 }
